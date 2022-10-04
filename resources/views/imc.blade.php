e12a4521a59304f5ea865553f0572f2eedb6b271
FIRST_ARACELI LAST_CRISTINA <araceli.melo@unidavi.edu.br>
2022-10-04 17:08:15 -0300

    finalizado

--- resources/views/imc.blade.php
+++ resources/views/imc.blade.php
@@ -1,22 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado análise IMC</title>
-</head>
-<body>
-    <main>
-        <h1>Resultado análise IMC</h1>
-        <div>
-            <h2>{{$resultadoimc["nome"]}}</h2>
-            <p>Idade: {{$resultadoimc["idade"]}}</p>
-            <p>Peso: {{$resultadoimc["peso"]}}</p>
-            <p>Altura: {{$resultadoimc["altura"]}}</p>
-            <p>IMC: {{$resultadoimc["imc"]}} - {{$resultadoimc["classificacaoimc"]}}</p>
-        </div>
-        <a href="{{url('/')}}">Voltar</a>
-    </main>
-</body>
-</html>
+
+@extends('layout.app')
+@section('title','Calculo IMC - Resultado')
+@section('apresentacao')
+    <p>Veja qual sua classificação com relação ao seu IMC</p>
+@endsection
+@section('content')
+    <div>
+        <h2>{{$resultadoimc["nome"]}}</h2>
+        <p>Idade: {{$resultadoimc["idade"]}}</p>
+        <p>Peso: {{$resultadoimc["peso"]}}</p>
+        <p>Altura: {{$resultadoimc["altura"]}}</p>
+        <p>IMC: {{$resultadoimc["imc"]}} - {{$resultadoimc["classificacaoimc"]}}</p>
+    </div>
+@endsection
--- resources/views/saude.blade.php
+++ resources/views/saude.blade.php
@@ -1,38 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplicação Saúde</title>
-</head>
-<body>
+@extends ('layout.app')
+@section ('title','Análises de saúde')
+@section ('content')
     <main>
-        <div>
-            <h1>Aplicação Saúde - Cálculo do IMC</h1>
-            <p>Esta Aplicação realiza o cálculo do IMC de uma pessoa, informando sua classificação com relação a este cálculo.</p>
-            <div>
-                <form action="{{url('/imc')}}" method="get">
-                    <label id="lnome" for="nome">Nome</label>
-                    <input type="text" name="nome" placeholder="Informe seu nome"required>
-                    <br/>
-                    <label id="ldatanascimento" for="datanascimento">Data de Nascimento</label>
-                    <input type="date" name="datanascimento" placeholder="Informe sua data de nascimento"required>
-                    <br/>
-                    <label id="lpeso" for="peso">Peso</label>
-                    <input type="number" step="0.01" name="peso" placeholder="Informe seu peso"required>
-                    <br/>
-                    <label id="latura" for="altura">Altura</label>
-                    <input type="number" step="0.01" name="altura" placeholder="Informe sua altura"required>
-                    
-                   <br/>
-                   <br/>
-                   <button type="submit" >Enviar</button>
+    @section ('titulo','Análises de saúde')
+    @section ('apresentacao')
+        <p>Possibilita analisar e calcular indicadores de saúde de uma pessoa. <br/> Como seu IMC e qualidade de sono. </p>
+        @endsection
 
-                </form>
-            </div>
-        </div>
-    </main>
-    
-</body>
-</html>
+        </p>
+    <main>
+    @endsection
